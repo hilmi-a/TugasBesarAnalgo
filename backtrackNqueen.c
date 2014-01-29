534ed2ae42e3395686006b3f5b0d40b873c07f77
hilmi-a <hilmi.a.aziz@gmail.com>
2014-01-29 12:40:39 +0700

    tambah main function

--- backtrackNqueen.c
+++ backtrackNqueen.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+int jml_queen;
 
 int main(int argc, char *argv[])
 {
-  
-  system("PAUSE");	
+  printf("\t\t\tN-Queen Problem Solving dengan Backtrack\n");
+  printf("\t\t\t========================================\n\n");
+  printf("\t\tMasukan Jumlah Queen : ");
+  scanf("%d",&jml_queen);
+  printf("\nSolusi yang Ditemukan:\n");
   return 0;
 }
