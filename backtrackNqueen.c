028fdf8a726ba4744b89b094160f195fb1a89626
Rinaldy Nursalis <ridy.salis@yahoo.co.id>
2014-01-30 11:03:24 +0700

    membuat fungsi penempatan

--- backtrackNqueen.c
+++ backtrackNqueen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int jml_queen;
+int papan[20];
 
 int main(int argc, char *argv[])
 {
@@ -34,3 +35,21 @@ void cetak_papan(int jml_queen,int q[])
  }
   
 }
+
+//Fungsi penempatan 
+int penempatan(int baris, int kolom)
+{
+     int i;
+     for (i=1;i<=baris-1;i++)
+     {
+//mengecek konflik kolom dan diagonal
+         if(papan[i]==kolom)
+           return 0;
+         else
+           if (abs(papan[i]-kolom)==abs(i-baris))
+               return 0;
+     }
+//tidak ada konflik
+     return 0;
+}
+           
