6efe8c1d70a0563c3fdd4dd060f6d8fed3751cd0
hilmi-a <hilmi.a.aziz@gmail.com>
2014-01-31 22:51:31 +0700

    edit menu utama

--- backtrackNqueen.c
+++ backtrackNqueen.c
@@ -6,17 +6,27 @@ int count;
 
 int main(int argc, char *argv[])
 {
+  int jml_queen,i,j;
+  void queen(int baris, int jml_queen);
+  
+  do{
+  system("cls");
   printf("\t\t\tN-Queen Problem Solving dengan Backtrack\n");
   printf("\t\t\t========================================\n\n");
-  printf("\t\tMasukan Jumlah Queen : ");
+  printf("\t\tMasukan Jumlah Queen [5..8]: ");
   scanf("%d",&jml_queen);
+  }while((jml_queen<5)||(jml_queen>8));
+  queen(1,jml_queen);
+  printf("\n\n\t\t\t\t======================");
+  printf("\n\t\t\t\tSOLUSI TELAH HABIS!!!\n\n");
+  system("pause");
   return 0;
 }
 
 //Fungsi membuat papan matriks
 void cetak_papan(int jml_queen)
 {
- //identifield
+ //identifier
  int i,j;
  
  //proses
@@ -48,6 +58,7 @@ void cetak_papan(int jml_queen)
 int penempatan(int baris, int kolom)
 {
      int i;
+
      for (i=1;i<=baris-1;i++)
      {
 //mengecek konflik kolom dan diagonal
@@ -58,12 +69,13 @@ int penempatan(int baris, int kolom)
                return 0;
      }
 //tidak ada konflik
-     return 0;
-     
+     return 1;
 }
+
 void queen(int baris, int jml_queen)
 {
      int kolom;
+     
      for(kolom=1;kolom<=jml_queen;kolom++)
      {
         if(penempatan(baris,kolom))
